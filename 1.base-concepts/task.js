3af90814fb75e9c392d65f5885f58f93fcf75066
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-06-28 21:20:16 +0300

    Решение

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    return arr;
+  } else if (D == 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+    return arr;
+  } else if (D > 0) {
+    let x1 = (-b - D ** 0.5) / (2 * a);
+    let x2 = (-b + D ** 0.5) / (2 * a);
+    arr.push(x1, x2);
+    return arr;
+  }
 }
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = +percent / 100 / 12;
+  let S = +amount - +contribution;
+  let n = +countMonths;
+  let Pay = S * (P + (P / (((1 + P) ** n) - 1)));//Ежемесячный платеж
+  let Payment = +(Pay * n).toFixed(2) || "False";//Общий платеж
+  return Payment;
 }
