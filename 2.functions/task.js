3556e7f8ae1d141fdf9ebc3bee4d5258b77cde2e
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-07-02 22:17:42 +0300

    Задание2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -42,9 +42,7 @@ function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
     let result = func(...arrOfArr[i]);
-    if (result > maxWorkerResult) {
-      maxWorkerResult = result;
-    }
+    (result > maxWorkerResult) && (maxWorkerResult = result);
   }
   return maxWorkerResult;
 }
