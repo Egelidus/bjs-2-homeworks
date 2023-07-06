f0c3c0e2f029c914863ab26c5e030d6502bf020d
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-07-06 21:10:27 +0300

    Задание3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
+"use strict"
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((a, b) => a === arr2[b]) && arr1.length === arr2.length;
+  return arr1.join() === arr2.join(); //Так короче.
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+   return users.some(
+     a => a.gender == gender
+   ) ? users.filter(
+     a => a.gender === gender
+   ).map(
+     a => a.age
+   ).reduce(
+     (a,b,c,d) => {
+       a += b;
+       return c == d.length-1 ? a/d.length : a;
+     }
+   ) : 0;
 }
