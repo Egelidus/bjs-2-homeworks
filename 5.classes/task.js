30b57884e4e33b2d718743089979d2e13da5cffd
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-07-30 17:45:53 +0300

    Задание5

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,40 +1,40 @@
 class PrintEditionItem{
-    constructor(name, releaseDate, pagesCount) {
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = 100;
-        this.type = null;
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+  fix() {
+    this.state *= 1.5;
+  }
+  set state(levelCrash) {
+    if (levelCrash < 0) {
+      this._state = 0}
+    else if (levelCrash > 100){
+      this._state = 100}
+    else {
+      this._state = levelCrash
     }
-    fix() {
-        this.state *= 1.5;
-    }
-    set state(levelCrash) {
-        if (levelCrash < 0) {
-          this._state = 0}
-        else if (levelCrash > 100){
-          this._state = 100}
-        else {
-          this._state = levelCrash
-        }
-    }
-    get state() {
-       return this._state;
-    }
+  }
+  get state() {
+    return this._state;
+  }
 }
 
 class Magazine extends PrintEditionItem{
-    constructor(name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-        this.type = "magazine";
-    }
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+      this.type = "magazine";
+  }
 }
 class Book extends PrintEditionItem{
-    constructor(author, name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = "book";
-      this.author = author;
-    }
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "book";
+    this.author = author;
+  }
 }
 class NovelBook extends Book{
   constructor(author, name, releaseDate, pagesCount) {
@@ -52,5 +52,21 @@ class DetectiveBook extends Book{
   constructor(author, name, releaseDate, pagesCount) {
     super(author, name, releaseDate, pagesCount);
     this.type = "detective";
+  }
+}
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+  addBook(book) {
+    book.state > 30 && this.books.push(book);
+  }
+  findBookBy(type, value) {
+    return this.books.find(item => item[type] == value) || null
+  }
+  giveBookByName(bookName) {
+    return this.books.findIndex(item => item.name == bookName) >= 0 ? this.books.splice(this.books.findIndex(item => item.name === bookName), 1) : null;
   }
 }
