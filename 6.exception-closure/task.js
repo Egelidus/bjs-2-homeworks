d9ea8b5954e78466aaa689121b82dc2026bddabb
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-08-09 23:36:30 +0300

    Задание6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+  if(+value) {
+    return Number.parseFloat(value)
+  } else {throw new Error("Невалидное значение")}
+}
+function validateCount(arg) {
+  try {
+    return parseCount(arg);
+  }
+  catch (error) {
+    return error
+  }  
+}
+class Triangle {
+  constructor(a, b, c) {
+    this.sideOne = a;
+    this.sideTwo = b;
+    this.sideThree = c;
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+  get perimeter () {
+    return this.sideOne + this.sideTwo + this.sideThree;
+  }
+  get area () {
+    let a = this.sideOne, b = this.sideTwo, c = this.sideThree, p = this.perimeter/2;
+    return +((p * (p - a) * (p - b) * (p - c))**0.5).toFixed(3);
+  }
+}
+function getTriangle (sideOne, sideTwo, sideThree) {
+  try {
+    return new Triangle (sideOne, sideTwo, sideThree);
+  } catch {
+    class TriError {
+      text () {
+        return "Ошибка! Треугольник не существует";
+      }
+      get perimeter () {
+        return this.text();
+      }
+      get area () {
+        return this.text();
+      }
+    }
+    return new TriError ()
+  }
+}
