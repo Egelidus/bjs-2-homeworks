0651683a986c8f652b0498c65be50b4976a35244
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-05 22:36:08 +0300

    Задание7

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,44 +3,44 @@ class AlarmClock {
     this.alarmCollection = [];
     this.intervalId = null;
   }
-  addClock (alarmTime, alarmGo) {
+  addClock(alarmTime, alarmGo) {
     if (!alarmTime || !alarmGo) {
       throw new Error("Отсутствуют обязательные аргументы")
     }
     if (this.alarmCollection.some(item => item.time == alarmTime)) {
       console.warn("Уже присутствует звонок на это же время");
     }
-    return this.alarmCollection.push({callback: alarmGo, time: alarmTime, canCall: true});
+    return this.alarmCollection.push({ callback: alarmGo, time: alarmTime, canCall: true });
   }
   removeClock(time) {
     this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
   }
-  getCurrentFormattedTime () {
-  let dateHH = new Date().getHours();
-  let dateMM = new Date().getMinutes();
-  let hours = String(dateHH).length == 1 ? `0${dateHH}` : dateHH;
-  let minutes = String(dateMM).length == 1 ? `0${dateMM}` : dateMM;
-  return `${hours}:${minutes}`;
+  getCurrentFormattedTime() {
+    let dateHH = new Date().getHours();
+    let dateMM = new Date().getMinutes();
+    let hours = String(dateHH).length == 1 ? `0${dateHH}` : dateHH;
+    let minutes = String(dateMM).length == 1 ? `0${dateMM}` : dateMM;
+    return `${hours}:${minutes}`;
   }
-  start () {
+  start() {
     if (this.intervalId) {
       return;
     }
     this.intervalId = setInterval(() => this.alarmCollection.forEach(item => {
       if (item.canCall && this.getCurrentFormattedTime() == item.time) {
         item.canCall = false;
-        return this.alarmCollection.callback()
+        item.callback()
       }
     }), 1000)
   }
-  stop () {
+  stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
   }
-  resetAllCalls () {
+  resetAllCalls() {
     this.alarmCollection.forEach(item => item.canCall = true);
   }
-  clearAlarms () {
+  clearAlarms() {
     this.stop();
     this.alarmCollection = [];
   }
